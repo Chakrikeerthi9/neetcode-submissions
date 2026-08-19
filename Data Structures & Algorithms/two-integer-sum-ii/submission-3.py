574eb0f55b10c33e89bcb5b72b6763c6e2b597b8
Chakri Keerthi <keerthichakri2148@gmail.com>
2026-08-19 02:13:17 -0500

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hm = {}
+        for i, n in enumerate(numbers):
+            if (target - n) in hm.keys():
+                return [i+1, hm[target - n]+1] if i < hm[target - n] else [hm[target - n]+1, i+1]
+            hm[n] = i
+        return []
